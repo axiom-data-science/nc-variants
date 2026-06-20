3d9731665fd8858553a969501b7a86e70a7e09d6
Shane St Savage <shane@axds.co>
2026-06-20 15:02:52 -0700

    Speed up processing by parallelizing ncks reads and short circuiting 100% consistent attributes

--- nc-variant-files.sh
+++ nc-variant-files.sh
@@ -37,4 +37,4 @@ fi
 TOKEN="$1"
 
 #give cat /dev/null so it doesn't hang trying to read stdin if no files matched
-cat /dev/null $(grep $GREP_FILE_LIST_ARG -RF "$TOKEN" $OUTDIR/*/nc.gron | sed 's/nc\.gron$/files/') | sort
+cat /dev/null $(grep $GREP_FILE_LIST_ARG -RF "$TOKEN" $OUTDIR/variants/*/nc.gron | sed 's/nc\.gron$/files/') | sort
